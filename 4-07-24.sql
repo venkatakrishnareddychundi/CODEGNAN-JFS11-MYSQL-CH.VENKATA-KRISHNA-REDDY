5c2839127bf5f8a304e9d2e6b1ec7f5ec42c43e2
Chundi Venkata Krishna reddy <130541187+venkatakrishnareddychundi@users.noreply.github.com>
2024-07-08 10:57:46 +0530

    Update 4-07-24.sql

--- 4-07-24.sql
+++ 4-07-24.sql
@@ -1,22 +1,22 @@
--- USE KRISHNA
--- CREATE TABLE VENKAT(ID INT,
--- PHONE BIGINT,
--- COURSE VARCHAR(10));
--- describe VENKAT;
--- describe user2;
--- use krishna
--- alter table venkat add email varchar(20);
--- desc venkat;
--- alter table venkat add (x1 int,x2 int);
--- desc venkat;
--- alter table venkat drop column x1;
--- alter table venkat drop column x2;
--- desc venkat;
--- alter table venkat modify column phone int;
--- desc venkat;
--- drop table user2;
--- select * from venkat;
--- select id,phone from venkat;
+USE KRISHNA
+CREATE TABLE VENKAT(ID INT,
+PHONE BIGINT,
+COURSE VARCHAR(10));
+describe VENKAT;
+describe user2;
+use krishna
+alter table venkat add email varchar(20);
+desc venkat;
+alter table venkat add (x1 int,x2 int);
+desc venkat;
+alter table venkat drop column x1;
+alter table venkat drop column x2;
+desc venkat;
+alter table venkat modify column phone int;
+desc venkat;
+drop table user2;
+select * from venkat;
+select id,phone from venkat;
 
 
 
